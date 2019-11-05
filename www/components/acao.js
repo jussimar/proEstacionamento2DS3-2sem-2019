12d5d6631f0c35c6544ba0b986dbf60a4111d6e0
jussimar <jussimar@jussimarleal.com.br>
2019-11-06 02:04:51 +0900

    ações de busca de veiculos, cadastro de clientes, cadastro de veiculos

--- www/components/acao.js
+++ www/components/acao.js
@@ -41,3 +41,97 @@ $(document).on("click","#cadProp",function(){
         }
     });    
 });
+
+$(document).on("click","#cadVeiculo",function(){
+    var parametros = {
+        "proprietario": $("option:selected",("#proprietario")).val(),
+        "placa": $("#placa").val(),
+        "tipo": $("option:selected",("#tipo")).val(),
+        "modelo": $("#modelo").val(),
+    };
+
+    $.ajax({
+        type:"post", //como enviar
+        url:webservice+"cadastra-veiculo.php",//para onde enviar
+        data:parametros,//o que enviar
+        //se der certo
+        success: function(data){
+            location.reload();
+
+        },
+        //se der errado
+        error: function(data){
+             navigator.notification.alert(data);
+        }
+    });    
+});
+
+
+$(document).on("click","#cadEstadia",function(){
+    var parametros = {
+        "entrada": $("#entrada").val(),
+        "veiculo": $("#codVeiculo").val(),
+    };
+
+    $.ajax({
+        type:"post", //como enviar
+        url:webservice+"cadastra-estadia.php",//para onde enviar
+        data:parametros,//o que enviar
+        //se der certo
+        success: function(data){
+            alert(data);
+            location.reload();
+
+        },
+        //se der errado
+        error: function(data){
+             navigator.notification.alert(data);
+        }
+    });    
+});
+
+
+/*busca de proprietarios*/
+function listarProprietario(){
+   $.ajax({
+        type:"post", //como enviar
+        url:webservice+"busca-proprietario.php",
+        dataType:"json",
+        //se der certo
+        success: function(data){
+            var itemlista = "";
+            $.each(data.proprietario,function(i,dados){
+              itemlista += "<option value='"+dados.codigo+"'>"+dados.nome+"</option>"; 
+            });
+        $("#proprietario").html(itemlista);
+        },
+        //se der errado
+        error: function(data){
+             navigator.notification.alert(data);
+        }
+    });    
+}
+
+
+$(document).on("click","#buscarVeiculo",function(){
+     var parametro ={
+      "placa":$("#placa").val()
+    };
+    
+    $.ajax({
+      type:"post",//como vou enviar os dados ao servidor
+      url:webservice+"busca-veiculo.php",
+      data:parametro,
+      dataType:"json",
+      //caso esteja tudo certo executa esse codigo
+      success: function(data){
+        $("#proprietario").val(data.veiculo.proprietario);
+        $("#tipo").val(data.veiculo.tipo);
+        $("#codVeiculo").val(data.veiculo.codigo);
+      },
+      //caso algo esteja errado executa esse codigo
+      error: function(data){
+        navigator.notification.alert("Erro ao buscar registros!");
+      }
+    });
+});
